72f9a1fa32e84437076cfad25e522bbf56bd1746
Mohd Waseem <mohdwaseem488@hotmail.com>
2019-02-18 00:12:46 +0530

    Add project files.

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE api/values/5
+        [CheckModelForNull]
+        public void Delete(int id)
+        {
+
+        }
+       
+        [CheckModelForInt]
+        [Route("test-int")]
+        public void CheckInt(int id)
+        {
+
+        }
+    }
+}
